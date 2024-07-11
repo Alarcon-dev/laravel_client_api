cc730ac6a41e8000c92ee307e3d9353a86e92bac
SJGDFPRGVW259\FORMACION <vessionmodel@gamail.com>
2024-07-11 09:15:44 -0500

    controller added

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Http;
+
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $url = env("URL_SERVER_API","/clients"); 
+        $reponse = Http::get($url); 
+        $data = $reponse->json(); 
+
+        return view('client', compact($data)); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
